6cffdd38532977a0889d9ff14fa88c559b8ecdc8
jacki-eee <jluisv1@students.towson.edu>
2025-04-12 19:06:48 -0400

    Landing Page
    Not done, but rough idea of landing page

--- the-park/src/App.js
+++ the-park/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import petImage from './petpic.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Change Change Change
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="landing">
+      <div className="image-container">
+        <img src={petImage} alt="Pet" />
+      </div>
+      <div className="text-container">
+        <h1>🌳 The Park 🌳</h1>
+        <p>A place to share all your favorite pet pictures with you family and friends!</p>
+        <button>Sign Up</button><br></br>
+        <button>Log In</button>
+      </div>
     </div>
   );
 }
-
 export default App;
