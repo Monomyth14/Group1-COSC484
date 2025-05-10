4348be59654abe0d5f0e67955c866af60dcc7d04
unknown <robowens0715@gmail.com>
2025-05-10 09:53:02 -0400

    Updated CreatePost.js

--- the-park/src/CreatePost.js
+++ the-park/src/CreatePost.js
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+import './Style/CreatePost.css';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from './Images/logo2.png';
+import createPostImage from './Images/CreatePostImage.avif';
+
+
 
 function CreatePost() {
   const [caption, setCaption] = useState('');
@@ -24,6 +30,18 @@ function CreatePost() {
   };
 
   return (
+    <div className="page-container">
+    <div className="sidebar">
+      <img src={logo} alt="Logo" />
+      <div className="nav">
+        <div>🏠 <Link to="/Main">Home</Link></div>
+        <div>👤 <Link to="/profile">My Profile</Link></div>
+        <div>👥 <Link to="/groupSignup">Create Group</Link></div>
+        <div>🔍 <Link to="/lost">Lost and Found</Link></div>
+        <div>🎉 <Link to="/petevents">Pet Events</Link></div>
+      </div>
+    </div>
+
     <div className="create-post-container">
       <h2>Create a Post</h2>
       <textarea
@@ -41,7 +59,11 @@ function CreatePost() {
       />
       <button className="login-button" onClick={handlePost}>Post</button>
       <button className="forgotpw-button" onClick={handleNotNow}>Not Now</button>
+
+      <img src={createPostImage} alt="Create Post" className="create-post-image" />
     </div>
+  </div>
+
   );
 }
 
